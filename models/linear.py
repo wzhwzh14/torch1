475fc28c3822d0b48ae5e922b330319a0880fcad
wzh <2846703175@qq.com>
2025-11-20 16:53:57 +0800

    upgrade

---
+++ models/linear.py
@@ -0,0 +1,49 @@
+import torch
+
+x_data = torch.tensor([[1.0],[2.0],[3.0]])
+y_data = torch.tensor([[2.0],[4.0],[6.0]])
+class LinearModel(torch.nn.Module):
+    def __init__(self):
+        super(LinearModel,self).__init__()
+        self.linear = torch.nn.Linear(1,1)
+    
+    def forward(self,x):
+        y_pred = self.linear(x)
+        return y_pred
+    #class torch.nn.Linear(in_features, out_features, bias=True)
+    #in_features: size of each input sample
+    #out_features: size of each output sample
+    #whether or not to add a bias(b)
+model = LinearModel()#callable model
+
+critertion = torch.nn.MSELoss(size_average=False)#loss
+
+optimizer = torch.optim.Rprop(model.parameters(),lr=0.04)
+
+for epoch in range(100):
+    #forward pass
+    y_pred = model(x_data)
+    
+    #compute loss
+    loss = critertion(y_pred,y_data)
+    print("epoch:",epoch,"loss:",loss.item())
+    
+    #zero gradients
+    optimizer.zero_grad()
+    
+    #backward pass
+    loss.backward()
+    
+    #update weights
+    optimizer.step()
+    #1. y hat
+    #2. loss function
+    #3. backward pass
+    #4. update weights
+
+print("w=",model.linear.weight.item())
+print("b=",model.linear.bias.item())
+
+x_test = torch.tensor([[4.0]])
+y_test = model(x_test)
+print("predict:",y_test.data)
